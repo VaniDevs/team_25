8eafaa785584f028bc868c4e49eec522a9498cde
ajyxc <ajyxcwdlydxwqy@gmail.com>
2018-09-22 15:11:33 -0700

    add modal

--- src/ui/Family.js
+++ src/ui/Family.js
@@ -1,24 +1,78 @@
 import React, { Component } from 'react';
-import { Form, Text, Radio, RadioGroup, TextArea, Checkbox } from 'react-form';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 
 class Family extends Component {
+	constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: true
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
 	render() {
-		return <Form render={({
-    submitForm
-  }) => (
-    <form onSubmit={submitForm}>
-      <Text field="firstName" placeholder='First Name' />
-      <Text field="lastName" placeholder='Last Name' />
-      <RadioGroup field="gender">
-        <Radio value="male" />
-        <Radio value="female" />
-      </RadioGroup>
-      <TextArea field="bio" />
-      <Checkbox field="agreesToTerms" />
-      <button type="submit">Submit</button>
-    </form>
-  )} />;
-	}
+
+
+		return(
+		<div>
+			<Modal
+			isOpen={this.state.modalIsOpen}
+			onAfterOpen={this.afterOpenModal}
+			onRequestClose={this.closeModal}
+			style={customStyles}
+			contentLabel="Example Modal"
+		>
+
+			<h2 ref={subtitle => this.subtitle = subtitle}>Family Info(Creating family form)</h2>
+
+
+			<form>
+				<h10>familyname</h10>
+				<input />
+				<h10>Date of Birth</h10>
+				<input />
+				<h10>Date of Birth of Baby</h10>
+				<input />
+				<h10>Phone Number</h10>
+				<input />
+				<h10>Email</h10>
+				<input />
+
+			</form>
+			<button onClick={this.closeModal}>save</button>
+		</Modal>
+			</div>
+  )
 }
+	}
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 export default Family;
