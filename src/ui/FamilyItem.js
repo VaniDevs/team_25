d0807f3c020d7b1ec5c06a569aecd9e47012ee2e
Nero Cui <nerocui@pop-os.localdomain>
2018-09-22 18:36:59 -0700

    added arrow

--- src/ui/FamilyItem.js
+++ src/ui/FamilyItem.js
@@ -3,12 +3,25 @@ import '../style/family.css';
 import Expand from 'react-expand-animated';
 
 class FamilyItem extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			open: false
+		};
+	}
+
 	render() {
 		return (
-			<div className="family_item">
-				<span className="family_item-child">{this.props.family_name}</span>
-				<span className="family_item-child">{this.props.phone_number}</span>
-				<span className="family_item-child">{this.props.status}</span>
+			<div>
+				<div className="family_item">
+					<span className="family_item-child">{this.props.family_name}</span>
+					<span className="family_item-child">{this.props.phone_number}</span>
+					<span className="family_item-child">{this.props.status}</span>
+					<button className="arrow-down" onClick={() => this.setState({ open: !this.state.open })} />
+				</div>
+				<Expand open={this.state.open}>
+					<h2>this.props.family_name</h2>
+				</Expand>
 			</div>
 		);
 	}
