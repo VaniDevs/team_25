02e1bec76e51e81f6277bf8e45ba6b43840fe6bc
ajyxc <ajyxcwdlydxwqy@gmail.com>
2018-09-23 08:38:20 -0700

    no message

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,13 @@
-import { combineReducers } from 'redux';
-import reducer_family from './reducer_families';
-import reducer_agencies from './reducer_agencies';
-import reducer_id from './reducer_id';
+import { combineReducers } from "redux";
+import reducer_family from "./reducer_families";
+import reducer_agencies from "./reducer_agencies";
+import reducer_id from "./reducer_id";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root_reducer = combineReducers({
-	families: reducer_family,
-	agencies: reducer_agencies,
-	id: reducer_id
+  families: reducer_family,
+  agencies: reducer_agencies,
+  id: reducer_id
 });
 
 export default root_reducer;
