d070c7db7bd35ddae635cfa45573a4cdbbf7fc1b
Nero Cui <nerocui@pop-os.localdomain>
2018-09-22 20:53:53 -0700

    added react posed

--- src/ui/Login.js
+++ src/ui/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import * as actions from '../actions';
 import { connect } from 'react-redux';
+import '../style/login.css';
 
 class Login extends Component {
 	login(name) {
@@ -9,13 +10,21 @@ class Login extends Component {
 
 	render() {
 		return (
-			<div>
-				<select>
+			<div className="login_box">
+				<select className="login_select login">
 					<option value="agency1">Agency 1</option>
 					<option value="agency2">Agency 2</option>
 				</select>
-				<input type="password" placeholder="Password" />
-				<button onClick={() => this.login('agency1')}>Login</button>
+				<input className="login_input login" type="password" placeholder="Password" />
+
+				<button
+					className="login_button login"
+					onClick={() => {
+						this.login('agency1');
+					}}
+				>
+					Login
+				</button>
 			</div>
 		);
 	}
